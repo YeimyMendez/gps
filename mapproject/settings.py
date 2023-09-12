3f0537b276a161641463a22255eb2769cf586cfb
YeimyMendez <fmendez5@misena.edu.co>
2023-09-12 09:00:09 +0000

    permisos especiales

--- mapproject/settings.py
+++ mapproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q&6-776h2-=9y5a84+fq29l=a+p#_9ns77!qte^+fj550#x5(w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['gelionz.pythonanywhere.com']
 
 # pythonanywhere.com
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
@@ -132,3 +132,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
